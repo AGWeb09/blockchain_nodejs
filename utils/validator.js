433e49fcaa1ed156b0fc0db6e8496372d749d1b3
a.gallo <andrea.gallo@altran.com>
2018-06-27 14:36:21 +0200

    Totally removed the console.log for debug purposes

--- utils/validator.js
+++ utils/validator.js
@@ -7,23 +7,18 @@ var ValidatorHandler = function ValidatorHandler(){
 
   function calculateHash(transaction){
     if(!transaction || transaction.sender === undefined || transaction.receiver === undefined){
-      // console.log("calculateHash: " , JSON.stringify(transaction), "false");
       return false;
     }
-    // console.log("calculateHash: " , JSON.stringify(transaction), "true", generateHashFromString(transaction.sender)+"-"+generateHashFromString(transaction.receiver)+"-0"+(new Date().getTime().toString(16)));
     return generateHashFromString(transaction.sender)+"-"+generateHashFromString(transaction.receiver)+"-0"+(new Date().getTime().toString(16));
   }
 
   function generateProof(transaction){
     if(!transaction || transaction.sender === undefined || transaction.receiver === undefined || transaction.amount === undefined){
-      // console.log("generateProof: " , JSON.stringify(transaction), "false");
       return false;
     }
     if(transaction.sender === 0){
-      // console.log("generateProof: " , JSON.stringify(transaction), "0", generateIntegerFromAddress(transaction.receiver) * parseFloat(transaction.amount));
       return generateIntegerFromAddress(transaction.receiver) * parseFloat(transaction.amount);
     }
-    // console.log("generateProof: " , JSON.stringify(transaction), "true" , Math.abs(generateIntegerFromAddress(transaction.sender)-generateIntegerFromAddress(transaction.receiver)) * parseFloat(transaction.amount));
     return Math.abs(generateIntegerFromAddress(transaction.sender)-generateIntegerFromAddress(transaction.receiver)) * parseFloat(transaction.amount);
   }
 
@@ -34,12 +29,10 @@ var ValidatorHandler = function ValidatorHandler(){
       hex = string.charCodeAt(i).toString(16);
       result += ("0"+hex).slice(-4);
     }
-    // console.log("generateHashFromString: ", string, result);
     return result;
   }
 
   function generateIntegerFromAddress(address){
-    // console.log("generateIntegerFromAddress: " , address, parseInt(address.match(/[0-9]+/g).join("")));
     return parseInt(address.match(/[0-9]+/g).join(""));
   }
 
